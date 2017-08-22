696aeb1c405acaa6faf92eabf64a0e291b8b4b2d
System <none>
2017-08-22 14:22:18 +0000

    Added search save and retrieve functionality

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Project to complete: https://www.freecodecamp.org/challenges/image-search-abstra
 
 
 Live demo at: https://magenta-sign.glitch.me/
+
+Recent searches at: https://magenta-sign.glitch.me/api/latest/imagesearch
+
+Example search: https://magenta-sign.glitch.me/api/imagesearch/lots%20and%20lots%20of%20cheese
+
+Search with page offset: https://magenta-sign.glitch.me/api/imagesearch/lots%20and%20lots%20of%20cheese?offest=4
--- server.js
+++ server.js
@@ -4,63 +4,100 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 var dotenv = require('dotenv');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = process.env.MONGOLAB_URI;
+
 
 dotenv.config();
 
 var GoogleSearch = require('google-search');
 var googleSearch = new GoogleSearch({
-  key: process.env.GOOGLE_CSE_KEY,
-  cx: process.env.GOOGLE_CSE_CX,
+    key: process.env.GOOGLE_CSE_KEY,
+    cx: process.env.GOOGLE_CSE_CX,
 });
 
-
+const SEARCHES = 'saved_searches'
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 
+
 app.route('/api/imagesearch/:query')
     .get(function(req, res) {
-      var output = {};
-      googleSearch.build({
-        q: req.params.query,
-        start: req.query.offset || 1,
-        searchType: "image",
-        num: 10, 
-      }, function(error, response) {
-        var images = [];
-        response.items.forEach(function(item){
-          images.push({url:item.link, snippet:item.snippet, context:item.image.contextLink})
+        var output = {};
+        googleSearch.build({
+            q: req.params.query,
+            start: req.query.offset || 1,
+            searchType: "image",
+            num: 10,
+        }, function(error, response) {
+            var images = [];
+            response.items.forEach(function(item) {
+                images.push({
+                    url: item.link,
+                    snippet: item.snippet,
+                    context: item.image.contextLink
+                })
+            });
+            dbInsert(SEARCHES, {
+                term: req.params.query,
+                when: Date()
+            }, function(error) {
+                if (error) throw error;
+            });
+            res.json(images);
         });
-        res.json(images);
-      });
 
     })
-  
+
+app.route('/api/latest/imagesearch/')
+    .get(function(req, res) {
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            db.collection(SEARCHES).find({}, {
+                limit: 50
+            }).toArray(function(err, searches) {
+                res.json(searches);
+            });
+        });
+    })
+
 app.route('/')
     .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
+        res.sendFile(process.cwd() + '/views/index.html');
     })
 
 // Respond not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
+app.use(function(req, res, next) {
+    res.status(404);
+    res.type('txt').send('Not found');
 });
 
 // Error Middleware
 app.use(function(err, req, res, next) {
-  if(err) {
-    res.status(err.status || 500)
-      .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }  
+    if (err) {
+        res.status(err.status || 500)
+            .type('txt')
+            .send(err.message || 'SERVER ERROR');
+    }
 })
 
 
 
 
-app.listen(process.env.PORT, function () {
-  console.log('Node.js listening ...');
+app.listen(process.env.PORT, function() {
+    console.log('Node.js listening ...');
 });
 
-
+function dbInsert(collection, dbObject) {
+    console.log("test")
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection(collection).insertOne(dbObject, function(err, result) {
+            if (err) throw err;
+            console.log('success')
+            db.close();
+        });
+    });
+}
